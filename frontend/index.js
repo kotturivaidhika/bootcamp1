d11c697db9b3d5167c1b47c0cad7daadb84bc961
kottturivaidhika <kotturivaidhika@gmail.com>
2026-08-13 16:34:49 +0530

    added functionality

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,25 @@
 //alert("welcom to my wesite")
+var users=[
+    {
+        "name": "John Doe",
+        "gender": "male",
+        "image":"john.png"
+    }  ,
+    {
+        "name": "Jane Smith",
+        "gender": "female",
+        "image":"jane.png"
+    }
+
+]
+var currentId=0;
+function toggleUser(){
+    currentId = (currentId + 1) % 2;
+    var image=document.getElementById("image1");
+    var name=document.getElementById("name1");
+    var gender=document.getElementById("gender1");
+    
+    name.innerHTML = users[currentId].name;
+    gender.innerHTML = users[currentId].gender;
+    image.src = users[currentId].image;
+}
