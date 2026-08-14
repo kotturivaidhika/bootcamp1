74c865abdb5aa263005033e9d55168787f57ea19
kottturivaidhika <kotturivaidhika@gmail.com>
2026-08-14 10:32:01 +0530

    added randomUser

--- frontend/index.js
+++ frontend/index.js
@@ -23,3 +23,24 @@ function toggleUser(){
     gender.innerHTML = users[currentId].gender;
     image.src = users[currentId].image;
 }
+function randomUser(){
+    fetch("https://randomuser.me/api/").then(
+        function(result){
+            return result.json();
+        }
+    ).then(
+        function(data){
+            var image=document.getElementById("image1");
+            var name=document.getElementById("name1");
+            var gender=document.getElementById("gender1");
+            name.innerHTML = data.results[0].name.first + " " + data.results[0].name.last;
+            gender.innerHTML = data.results[0].gender;
+            image.src = data.results[0].picture.large;
+            // Handle the JSON data here
+        }
+    ).catch(
+        function(error){
+            console.log("Error fetching random user:", error);
+        }
+    );
+}
